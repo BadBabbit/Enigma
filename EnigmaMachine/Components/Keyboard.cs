bdf641c0396571b7f8a3e03774755f37d9445baa
Nevan Masterson <nevanmasterson1@gmail.com>
2025-07-27 15:18:28 +0100

    Add project files.

---
+++ EnigmaMachine/Components/Keyboard.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Keyboard
+    {
+        /// <summary>
+        /// The set of keys that should be enciphered through the Enigma machine (is the set of letters in the
+        /// English alphabet).
+        /// </summary>
+        private static readonly string encipheredKeys = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+        /// <summary>
+        /// This class represents a single keystroke input. Defines behaviour for whether or not a given
+        /// key should be encrypted via the machine.
+        /// </summary>
+        /// <param name="key">The value of the keystroke.</param>
+        /// <param name="encipher">Whether the key should be enciphered through the machine.</param>
+        /// <param name="terminator">Whether this key represents the end of the input stream.</param>
+        public class Input(char key, bool encipher, bool terminator)
+        {
+            private readonly char _key = key;
+            /// <summary>
+            /// Getter for Key property.
+            /// </summary>
+            public char Key {
+                get { return _key; }
+            }
+
+            private readonly bool _encrypt = encipher;
+            /// <summary>
+            /// Getter for Encrypt property.
+            /// </summary>
+            public bool Encrypt
+            {
+                get { return _encrypt; }
+            }
+
+            private readonly bool _terminator = terminator;
+            /// <summary>
+            /// Getter for Terminator property.
+            /// </summary>
+            public bool Terminator
+            {
+                get { return _terminator; }
+            }
+
+        }
+        public Keyboard() { }
+        
+        public Input GetSingleKeyInput()
+        {
+            ConsoleKeyInfo cki = Console.ReadKey(true);
+            char key = Char.ToUpper(cki.KeyChar);
+            bool encrypt = encipheredKeys.Contains(key);
+            bool terminator = cki.Key == ConsoleKey.Enter;
+            return new Input(key, encrypt, terminator);
+        }
+    }
+}
+    
---
+++ EnigmaMachine/EnigmaMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Enigma.Components;
+
+namespace Enigma
+{
+    public class EnigmaMachine
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a key:");
+            Keyboard keyboard = new Keyboard();
+            while (true)
+            {
+                Keyboard.Input input = keyboard.GetSingleKeyInput();
+                Console.Write(input.Key);
+                if (input.Terminator) break;
+            }
+        }
+    }
+}
