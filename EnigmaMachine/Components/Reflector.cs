3c77874a7c15c9ed5f4099103aea537dfc089084
BadBabbit <nevanmasterson1@gmail.com>
2025-12-26 14:52:29 +0000

    boilerplate reflector

---
+++ EnigmaMachine/Components/Reflector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.Components
+{
+    internal class Reflector
+    {
+        private char[] chars;
+        private int position;
+        public Reflector(string chars, int position)
+        {
+            this.chars = chars.ToCharArray();
+            this.position = position;
+        }
+    }
+}
