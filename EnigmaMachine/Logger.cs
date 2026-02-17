aa5c646ab582777e62b79a5349a4b63e1820f769
Nevan Masterson <nevanmasterson1@gmail.com>
2026-02-17 19:17:32 +0000

    created a logger

---
+++ EnigmaMachine/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace Enigma;
+
+internal class Logger
+{
+    private static object _lock = new object();
+    private static ILogger instance;
+    private static readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.SetMinimumLevel(LogLevel.Debug).AddConsole();
+    });
+
+    public static ILogger GetInstance()
+    {
+        if (instance == null)
+        {
+            lock (_lock)
+            {
+                if (instance == null)
+                {
+                    instance = _loggerFactory.CreateLogger("Enigma");
+                }
+            }
+        }
+        return instance;
+    }
+}
